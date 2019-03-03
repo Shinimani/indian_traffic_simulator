38c41059926bacc1fe65ba3bf04c5d866e96600b
Navneel Mandal <navneel99@gmail.com>
2019-03-04 00:49:38 +0530

    specific type now a vehicle attribute

--- code_file/vehicle.cpp
+++ code_file/vehicle.cpp
@@ -1,3 +1,18 @@
 #include "vehicle.hpp"
 
 //Here we will add functions of the vehicle class
+void Vehicle::setType(string type){
+    vehicle_type = type;
+}
+
+void Vehicle::setBasicAttributes(int l, int w, int acc, int dec, float initSpeed){
+    length = l;
+    width = w;
+    acceleration = acc;
+    decelaration = dec;
+    brake = false;
+    speed = initSpeed;
+}
+void Vehicle::setColour(string c){
+    colour = c;
+}
--- headers/vehicle.hpp
+++ headers/vehicle.hpp
@@ -3,8 +3,11 @@
 
 #include "common.hpp"
 
+// enum {car, bike, bus, truck}
+
 class Vehicle{
-    protected:
+    protected:  
+            string vehicle_type; //Can change into enum type afterwards
             string colour;
             int length;
             int width;
@@ -13,6 +16,12 @@ class Vehicle{
             float speed;
             float decelaration; //Will come into play if brake = true
             float x,y; //location of the top left of the vehicle
+    public:
+        void Vehicle::setType(string type);
+        void Vehicle::setBasicAttributes(int l, int w, int acc, int dec, float initSpeed);
+        void Vehicle::setColour(string c);
+
+
 
 };
 
