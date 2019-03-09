bf4ef3bfe74d4baaaceceae80319be73bb5532e9
Navneel Mandal <navneel99@gmail.com>
2019-03-09 12:45:56 +0530

    added a generic function

--- headers/common.hpp
+++ headers/common.hpp
@@ -8,4 +8,19 @@
 #include<string> 
 using namespace std;
 
+template<typename T>
+bool commIn2vectors(vector<T> a, vector<T> b){
+    for (int i = 0; i<a.size(); i++){
+        for (int j = 0; j<b.size();j++){
+            if (a[i] == b[j]){
+                return false;
+                break;
+            }else{
+                continue;
+            }
+        }
+    }
+    return true;
+}
+
 #endif
